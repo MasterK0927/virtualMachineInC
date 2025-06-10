a11f69b88c61c1952de1aad258dd4244e070f3cc
MasterK0927 <keshav.agarwal.12325@gmail.com>
2025-06-10 10:10:00 +0530

    feat: introduce decoder interfaces

---
+++ include/vm/Decoder.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "vm/Types.hpp"
+#include "vm/Opcodes.hpp"
+
+namespace vm {
+
+struct IMemory;
+
+struct DecodedInst {
+    Opcode op{Opcode::HALT};
+    u8 a{0};
+    u8 b{0};
+    u32 imm{0};
+    u8 size{1};
+};
+
+struct IDecoder {
+    virtual ~IDecoder() = default;
+    virtual DecodedInst decode(const IMemory& mem, u32 pc) const = 0;
+};
+
+class SimpleDecoder : public IDecoder {
+public:
+    DecodedInst decode(const IMemory& mem, u32 pc) const override;
+};
+
+} // namespace vm
